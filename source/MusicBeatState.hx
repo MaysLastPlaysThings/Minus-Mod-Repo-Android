fc078650bf0527130eccfc9111a638ff1a377b19
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-09-10 21:27:49 +0300

    update source/MusicBeatState.hx

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -99,6 +99,8 @@ class MusicBeatState extends FlxUIState
 	{
 		if (virtualPad != null)
 		{
+      virtualPad.x = x;
+      virtualPad.y = y;
 			var camControls:FlxCamera = new FlxCamera();
 			FlxG.cameras.add(camControls, DefaultDrawTarget);
 			camControls.bgColor.alpha = 0;
