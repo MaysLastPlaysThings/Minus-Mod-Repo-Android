146f7ef12453c1e55089dace4ab00aa61a7840f3
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2024-09-10 18:25:25 +0300

    come on

--- source/Main.hx
+++ source/Main.hx
@@ -9,7 +9,7 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, TitleState, 1, 60, 60, true));
+		addChild(new FlxGame(1280, 720, TitleState, 1, 60, 60, true));
 
 		addChild(new FPS(10, 3, 0xFFFFFF));
 	}
